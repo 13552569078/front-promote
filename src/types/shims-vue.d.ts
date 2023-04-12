d690a8a74c91805f1f08c7bc5ccf0e68e3d69ef4
zhanghongjie <zhanghongjie@cestc.cn>
2023-04-12 15:43:00 +0800

    feat: 类型

---
+++ src/types/shims-vue.d.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
